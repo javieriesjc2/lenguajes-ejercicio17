86457da76cd5f90ec3ac6152f22302cf0118c40d
Lorigame <davidsans009@gmail.com>
2026-01-29 18:38:43 +0100

    Añadir el filtro

--- david/ej17.js
+++ david/ej17.js
@@ -16,23 +16,28 @@ function CargarDatosEnTabla() {
         for (var i = 0; i < biblioteca.length; i++) {
             var libro = biblioteca[i];
 
-            var row = tableBody.insertRow();
+            if (document.getElementById("filtro").value == libro.getElementsByTagName("anio")[0].textContent || document.getElementById("filtro").value == "") {
+                var row = tableBody.insertRow();
 
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
 
-            cell1.textContent =
-                libro.getElementsByTagName("titulo")[0].textContent;
-            cell2.textContent =
-                libro.getElementsByTagName("autor")[0].textContent;
-            cell3.textContent =
-                libro.getElementsByTagName("anio")[0].textContent;
-            cell4.textContent =
-                libro.getElementsByTagName("precio")[0].textContent;
+                cell1.textContent =
+                    libro.getElementsByTagName("titulo")[0].textContent;
+                cell2.textContent =
+                    libro.getElementsByTagName("autor")[0].textContent;
+                cell3.textContent =
+                    libro.getElementsByTagName("anio")[0].textContent;
+                cell4.textContent =
+                    libro.getElementsByTagName("precio")[0].textContent;
+            }
+
         }
+
     }
+
 }
 
 // Cargar función
