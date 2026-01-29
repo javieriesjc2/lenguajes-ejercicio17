202a440b8519623f448be697322459cd746ac127
Ethanedc <ethanedcws@gmail.com>
2026-01-29 17:47:39 +0100

    organizar mi carpeta (porque no me dices eso javi)

---
+++ ethan/util.js
@@ -0,0 +1,109 @@
+let TITLE_INDEX = 0;
+let AUTOR_INDEX = 1;
+let YEAR_INDEX = 2;
+let PRICE_INDEX = 3;
+
+// Source - https://stackoverflow.com/a
+// Posted by Etienne Martin, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-01-23, License - CC BY-SA 4.0
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
+
+function getXmlData(fn, func)
+{
+    var req = new XMLHttpRequest();
+    req.open("GET", fn, false);
+    req.send();
+
+    if(req.status === 200)
+    {
+        var xDoc = req.responseXML;
+        func(xDoc);
+    }
+}
+
+function clearData()
+{
+    let tab = document.querySelector("table.container");
+    while(tab.children.length > 1)
+    {
+        tab.removeChild(tab.children[1]);
+    }
+}
+
+function getValueFromComparator(a, b)
+{
+    const op = document.querySelector("#filter-op").value;
+    switch(op)
+    {
+    case "==": return a == b;
+    case "!=": return a != b;
+    case ">":  return a >  b;
+    case ">=": return a >= b;
+    case "<":  return a <  b;
+    case "<=": return a <= b;
+    default: return true;
+    }
+}
+
+function getData()
+{
+    getXmlData("../libros.xml", async (_xDoc) =>
+    {
+        /**
+         * @type Document
+         */
+        var xDoc = _xDoc;
+        const libros = xDoc.getElementsByTagName("libros")[0].children;
+
+        var filterYear = document.querySelector("#filter").value;
+
+        let tab = document.querySelector("table.container");
+        clearData();
+
+        for(let i = 0; i < libros.length; i++)
+        {
+            const libro = libros[i];
+            const title = libro.children[TITLE_INDEX].textContent;
+            const autor = libro.children[AUTOR_INDEX].textContent;
+            const year = libro.children[YEAR_INDEX].textContent;
+            const price = libro.children[PRICE_INDEX].textContent;
+
+            if((filterYear !== "" && document.querySelector("#filter-op").value !== "") &&
+               !getValueFromComparator(parseInt(year), parseInt(filterYear)))
+            {
+                continue;
+            }
+
+            var entry = document.createElement("tr");
+
+            var tData = document.createElement("td");
+            tData.textContent = title;
+            entry.appendChild(tData);
+
+            var aData = document.createElement("td");
+            aData.textContent = autor;
+            entry.appendChild(aData);
+
+            var yData = document.createElement("td");
+            yData.textContent = year;
+            entry.appendChild(yData);
+
+            var pData = document.createElement("td");
+            pData.textContent = price;
+            entry.appendChild(pData);
+
+            tab.appendChild(entry);
+
+            // await delay(200 * i);
+        }
+
+    });
+}
+
+window.addEventListener("load", () =>
+{
+    getData();
+});
+
