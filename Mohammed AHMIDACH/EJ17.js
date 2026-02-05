05b7f3bc09f4c8b865d30b7aca1b5b2fb6c630a2
Mohammed <mohahm7@alu.edu.gva.es>
2026-02-05 17:55:40 +0100

    commit3

--- Mohammed AHMIDACH/EJ17.js
+++ Mohammed AHMIDACH/EJ17.js
@@ -1,7 +1,7 @@
 function CargarDatosEnTabla() {
 // Carga el archivo XML
 var request = new XMLHttpRequest();
-request.open("GET", "libros.xml", false);
+request.open("GET", "../libros.xml", false);
 request.send();
 // Comprueba que se encontró el archivo
 if (request.status === 200) {
@@ -22,7 +22,7 @@ var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 var cell4 = row.insertCell(3);
-// Se cambia el contenido de cada celda con los datos del participante
+// Se cambia el contenido de cada celda con los datos del libro
 cell1.textContent = libro.getElementsByTagName("titulo")[0].textContent;
 cell2.textContent = libro.getElementsByTagName("autor")[0].textContent;
 cell3.textContent = libro.getElementsByTagName("anio")[0].textContent;
@@ -36,7 +36,7 @@ CargarDatosEnTabla();
 function Filtro() {
 // Carga el archivo XML
 var request = new XMLHttpRequest();
-request.open("GET", "libros.xml", false);
+request.open("GET", "../libros.xml", false);
 request.send();
 // Comprueba que se encontró el archivo
 if (request.status === 200) {
@@ -62,7 +62,7 @@ var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 var cell4 = row.insertCell(3);
-// Se cambia el contenido de cada celda con los datos del participante
+// Se cambia el contenido de cada celda con los datos del libro
 cell1.textContent = libro.getElementsByTagName("titulo")[0].textContent;
 cell2.textContent = libro.getElementsByTagName("autor")[0].textContent;
 cell3.textContent = libro.getElementsByTagName("anio")[0].textContent;
